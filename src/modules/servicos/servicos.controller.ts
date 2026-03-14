22540fc76fb884a51b625170e7d3c65146bdad0e
IsabeleLeticiaQueiroz <bbelequeiroz@gmail.com>
2026-03-14 14:26:09 -0300

    feat: implementa endpoints GET /servicos e GET /servicos/:id

--- src/modules/servicos/servicos.controller.ts
+++ src/modules/servicos/servicos.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { ServicosService } from './servicos.service';
 
 @Controller('servicos')
 export class ServicosController {
   constructor(private readonly servicosService: ServicosService) {}
+
+  @Get()
+  async findAll() {
+    return this.servicosService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.servicosService.findOne(id);
+  }
 }
